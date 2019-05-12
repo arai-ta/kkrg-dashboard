3cf77be92c00b7152c100e2608f77e1a8075b2cb
Arai Takashi <takashi.arai.nz@icloud.com>
2019-05-13 00:21:59 +0900

    Add image example

--- Dashboard.js
+++ Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { connect } from "@dashbling/client/dashbling";
 import { Dashboard } from "@dashbling/client/components";
-import { Clock } from "@dashbling/client/widgets";
+import { Clock, Image } from "@dashbling/client/widgets";
 import { HelloWidget } from "./widgets/HelloWidget";
 import { GitHubStars } from "./widgets/gitHubStars/GitHubStars";
 import { CircleCiStatus } from "./widgets/circleCi/CircleCiStatus";
@@ -21,6 +21,7 @@ export default props => {
         timezone="Europe/Amsterdam"
         backgroundColor="#00865A"
       />
+      <Image src="https://static.tenki.jp/static-images/radar/2019/05/13/00/05/00/pref-22-large.jpg" />
       <BoundHelloWidget name="arai-ta"/>
 
       <WeatherInAmsterdam title="Amsterdam" />
